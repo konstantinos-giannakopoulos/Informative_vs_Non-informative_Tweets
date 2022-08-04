e287f83573e0b8dc88e1ba7a94969ef2f93afd5f
Konstantinos Giannakopoulos <82231365+konstantinos-giannakopoulos@users.noreply.github.com>
2022-08-04 16:17:16 +0800

    First Commit. Add Lexical.

---
+++ InfoTweetsLexical/src/hk/ust/cse/kostas/infotweetslexical/client/InfoTweetsLexical.java
@@ -0,0 +1,84 @@
+package infotweetslexical.client;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import infotweetslexical.lexical.Tokenizer;
+import infotweetslexical.lexical.Trimmer;
+import infotweetslexical.lexical.Fixer;
+import infotweetslexical.lexical.Stemmer;
+
+public class InfoTweetsLexical {
+
+    Tokenizer tokenizer;
+    Trimmer trimmer;
+    Fixer fixer;
+    Stemmer stemmer;
+
+    public InfoTweetsLexical() {
+	this.tokenizer = new Tokenizer();
+	this.trimmer = new Trimmer();
+	this.fixer = new Fixer();
+	this.stemmer = new Stemmer();
+    }
+
+    private List<String> tokenize(String message) {
+	return (this.tokenizer.tokenizeText(message));
+    }
+
+    /**
+     * Tokenizes the tweet message. Keeps untangled 
+     * hashtags and meantined users. 
+     */
+    public List<String> process(String message) {
+	/*
+	  1) Tokenize the input string.
+	  2) Trim it, by removing spaces, and punctuation.
+	  3) Slightly modify it.
+	  4) Stem.
+	 */
+	List<String> result = new ArrayList<String>();
+	List<String> tokens = this.tokenize(message);
+	for(String token : tokens) {
+	    if(token.startsWith("#")) {
+		result.add(token);
+	    } else if (token.contains("@")) {
+		while(!token.startsWith("@")) {
+		    token = token.substring(1, token.length());
+		}
+		if(token.endsWith(":")) {
+		    token = token.substring(0, token.length() - 1);
+		}
+		result.add(token);
+	    } else {
+		String trimstr = this.trimmer.trim(token);
+		String fixstr = this.fixer.fix(trimstr);
+		String stemsrt = this.stemmer.stem(fixstr);
+		if(!stemsrt.equals(""))
+		    result.add(stemsrt);		
+	    }
+	}
+	return result;
+    } // process
+
+    public static void main(String[] args) {
+	InfoTweetsLexical lexical = new InfoTweetsLexical();
+	List<String> messages = new ArrayList<String>();
+	messages.add( "Hi! My name, is Kostas... How are u?");
+	messages.add("http://www.hello.com...");
+	messages.add("heeeelllllooooooo");
+	messages.add("#sleep.");
+	messages.add("was");
+	messages.add("I went to the @gym at 3 o'clock. Then I came home.");
+	messages.add("where");
+	messages.add("where...are");
+	messages.add("Cant I wanna going.. there? with you???");
+	messages.add("hi hey");
+	System.out.println("--> Tokenizer");
+
+	for(String message : messages) {
+	    System.out.println(lexical.process(message));
+	}
+    }
+
+} // InfoTweetsLexical
